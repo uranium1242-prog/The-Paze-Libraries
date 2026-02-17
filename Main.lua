26128ee20bc37c7e48461510b90ebf72b4bb7196
uranium1242-prog <uranium1242@gmail.com>
2026-02-17 16:30:37 -0500

    Main.lua

--- Main.lua
+++ Main.lua
@@ -2,14 +2,13 @@ local PlaceId = game.PlaceId
 
 local ScriptList = {
     [2753915549] = "https://raw.githubusercontent.com/uranium1242-prog/The-Paze-Libraries/refs/heads/main/BloxFruits.lua",
-    [126884695634066] = "https://raw.githubusercontent.com/uranium1242-prog/The-Paze-Libraries/refs/heads/main/Fisch.lua",
     [79546208627805] = "https://raw.githubusercontent.com/uranium1242-prog/The-Paze-Libraries/refs/heads/main/99NightsInTheForest.lua",
-    [17867589736] = "https://raw.githubusercontent.com/uranium1242-prog/The-Paze-Libraries/refs/heads/main/EscapeTsunami.lua",
+    [126884695634066] = "https://raw.githubusercontent.com/uranium1242-prog/The-Paze-Libraries/refs/heads/main/GrowAGarden.lua",
 }
 
 local targetScript = ScriptList[PlaceId]
 if targetScript then
     loadstring(game:HttpGet(targetScript))()
 else
-    warn("PazeLibs: No script found for this game.")
+    warn("PazeLibs: No script found for this game (PlaceId: " .. tostring(PlaceId) .. ")")
 end
